b89eaaea0f811ec8ded3b5b13ab8e6655d255516
Azamat Zulpykhar <augustintherome@gmail.com>
2022-10-03 02:32:32 +0600

    feat: tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function Index() {
-  return <Link href={{ pathname: "/" }}>Index</Link>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
