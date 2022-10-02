3f88fcce7b107885cf7f16ca3c90b19b09f529db
Azamat Zulpykhar <augustintherome@gmail.com>
2022-10-03 02:51:36 +0600

    feat: added logo component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { Logo } from "app/core/components";
+
 export default function Index() {
   return (
     <h1 className="text-3xl font-bold underline font-syne">
-      <span className="font-manrope">Hello</span> world!
+      <span className="font-manrope">Hello</span> world! <Logo />
     </h1>
   );
 }
