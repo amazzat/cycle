8c9bebc09d10971a1b0351fae342a6ad69c2ab25
Azamat Zulpykhar <augustintherome@gmail.com>
2022-10-03 02:52:13 +0600

    chore: renamed logo to logo icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { Logo } from "app/core/components";
+import { LogoIcon } from "app/core/components";
 
 export default function Index() {
   return (
     <h1 className="text-3xl font-bold underline font-syne">
-      <span className="font-manrope">Hello</span> world! <Logo />
+      <span className="font-manrope">Hello</span> world! <LogoIcon />
     </h1>
   );
 }
